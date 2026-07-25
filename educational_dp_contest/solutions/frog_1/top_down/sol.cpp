2fe9365ccdc9da4b6a588d7ff444e12cbb3c19a5
Hemant Ingle <hemantingle1999@gmail.com>
2026-07-25 17:25:56 +0530

    Fix

--- educational_dp_contest/solutions/frog_1/top_down/sol.cpp
+++ educational_dp_contest/solutions/frog_1/top_down/sol.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e9;
-
-int frog_1(int N, vector<int> &h, vector<int> &dp) {
-    if(N==0) return 0;
-    if(N==1) return abs(h[1] - h[0]);
-    if(dp[N] != INF) return dp[N];
-    dp[N] = min(frog_1(N-1, h, dp) + abs(h[N] - h[N-1]),  frog_1(N-2, h, dp) + abs(h[N] - h[N-2]));
-    return dp[N];
+int solve(int i, vector<int> &h, vector<int> &dp) {
+    if(i==0) return 0;
+    if(i==1) return abs(h[1] - h[0]);
+    if(dp[i] != -1) return dp[i];
+    return dp[i] = min(
+        solve(i-1, h, dp) + abs(h[i] - h[i-1]),  
+        solve(i-2, h, dp) + abs(h[i] - h[i-2])
+    );
 }
 
 int main()
@@ -20,8 +20,8 @@ int main()
     vector<int> h(N);
     for(int &x: h) cin>>x;
 
-    vector<int> dp(N, INF);
-    int ans = frog_1(N-1, h, dp);
+    vector<int> dp(N, -1);
+    int ans = solve(N-1, h, dp);
     cout<<ans<<"\n";
     return 0;
 }
